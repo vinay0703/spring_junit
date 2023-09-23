2a15b9865b6c287a64bb495394ccd03d35481e8c
vinay0703 <vinay.araveti@gmail.com>
2023-09-23 11:34:02 +0530

    Fixed lombok errors

--- src/main/java/com/example/junit/entity/Book.java
+++ src/main/java/com/example/junit/entity/Book.java
@@ -4,24 +4,22 @@ import lombok.*;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Books")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NonNull
+    @NotNull
     private String name;
 
-    @NonNull
+    @NotNull
     private String summary;
 
-    @NonNull
     private int rating;
 }
